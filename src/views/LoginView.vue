f4533697725fb5285b78fcfba5870ea71422ca6f
Gabriel-VitorS <gabrielvitordasilva@gmail.com>
2023-03-04 15:40:39 -0300

    Tela de login

---
+++ src/views/LoginView.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="container">
+        <!-- <section class="row justify-content-center"> -->
+            <span class="fs-1 col-4" >Login</span>
+        <!-- </section> -->
+        
+        <section class="row justify-content-center mt-3">
+            
+            <FormLogin />
+        </section>
+    </div>
+
+</template>
+
+<script>
+import FormLogin from '@/components/gestor/login/FormLogin.vue';
+export default {
+    name: 'LoginView',
+    components: {FormLogin}
+}
+</script>
