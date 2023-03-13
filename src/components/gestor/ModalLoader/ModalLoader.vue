9702d5782ba1bef7f1955c078138917be55c3b3b
Gabriel-VitorS <gabrielvitordasilva@gmail.com>
2023-03-13 18:07:25 -0300

    Componete de loader

---
+++ src/components/gestor/ModalLoader/ModalLoader.vue
@@ -0,0 +1,27 @@
+<template>
+    <div ref="modalLoader" class="modal" id="modalLoader" data-bs-backdrop="static" tabindex="-1">
+      <div class="modal-dialog modal-dialog-centered" style="justify-content: center;">
+        <span class="spinner-border text-light" role="status" aria-hidden="true">
+            <span class="visually-hidden">Loading...</span>
+        </span>
+      </div>
+    </div>
+</template>
+
+<script>
+import { Modal } from 'bootstrap';
+
+export default {
+    name: 'modalLoader',
+    mounted(){
+        const loader = new Modal(document.querySelector('#modalLoader'))
+        loader.show()
+    },
+    beforeUnmount(){
+        
+        const loader = new Modal(document.querySelector('#modalLoader'))
+        loader.dispose()
+        document.querySelector('.modal-backdrop').remove()
+    }
+}
+</script>
