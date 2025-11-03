96b883112434bed0012d3a748e0363a11b39934a
Gabriel Vitor <gabrielvitordasilva@gmail.com>
2025-11-03 14:04:42 -0300

    btn com spinner

---
+++ src/components/common/BtnWithSpinner.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import SpinnerSm from '@/components/ui/SpinnerSm.vue';
+defineProps<{
+    isSending: boolean
+    type: 'btn-primary' | 'btn-danger' | 'btn-close' | 'btn-secondary' | 'btn-success'
+}>()
+
+</script>
+<template>
+    <button class="btn" :class="type" :disabled="isSending">Salvar <SpinnerSm v-if="isSending" class="ms-1"></SpinnerSm> </button>
+</template>
