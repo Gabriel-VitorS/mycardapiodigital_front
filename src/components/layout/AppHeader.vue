b1218f3c479b9cd3a01e74c7cc4544469e5bdb19
Gabriel Vitor <gabrielvitordasilva@gmail.com>
2025-12-09 17:24:08 -0300

    component header

--- src/components/layout/AppHeader.vue
+++ src/components/layout/AppHeader.vue
@@ -4,6 +4,8 @@ import {getCompany} from '@/api/manager/company'
 import { Toast } from '@/stores/toast';
 import type { CompanyResponse } from '@/types/manager/company.type';
 import {useTheme} from "@/components/layout/ThemeProvider.vue"
+import { useMenuSideBar } from '@/stores/menuSideBarStore';
+const menuSideBarStore = useMenuSideBar()
 
 const {toggleTheme, isDarkMode} = useTheme()
 const company = ref<CompanyResponse>()
@@ -23,38 +25,55 @@ onMounted( async () => {
 </script>
 
 <template>
-    <header class="border border-subtle py-2 pt-2">
-        <div class="container-fluid d-grid gap-3 align-items-center" style="grid-template-columns: 1fr 2fr;">
+    <header class="border-bottom border-subtle py-3 pt-2">
+        <div class="container-fluid ">
+            <div class="row">
 
-            
-            <div>
-                <router-link to="/gestor">
-                    <img v-if="isDarkMode" src="/images/mycardapiodigital_light.svg" alt="logo" width="200">
-                    <img v-else src="/images/mycardapiodigital_dark.svg" alt="logo" width="200">
-                    
-                </router-link>
-            </div>
-
-            <div class="d-flex justify-content-end align-items-center">
-
-                <div class="me-3"  @click="toggleTheme" role="button">
-                    
-                    <i v-if="isDarkMode" class="bi bi-brightness-high-fill"></i>
-                    <i v-else class="bi bi-moon-stars-fill"></i>
+                <div class="col">
+                    <div class="border text-center d-md-none d-block" style="border-radius: 5px; width: 45px;" role="button"  @click="menuSideBarStore.toggleMenu()">
+                        <i class="bi bi-list fs-4"></i>
+                    </div>
+                </div>
+                
+                <div class="col">
+                    <router-link to="/gestor">
+                        <div class="d-md-none d-block">
+                            <img v-if="isDarkMode" src="/images/mycardapiodigital_light.svg" alt="logo">
+                            <img v-else src="/images/mycardapiodigital_dark.svg" alt="logo" >
+                            
+                        </div>
+                        
+                    </router-link>
                 </div>
 
+                <div class="col d-flex justify-content-end align-items-center">
 
-                <div class="dropdown">
-                    <a class="btn btn-light dropdown-toggle" :class="{'btn-dark': isDarkMode}" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                        {{company?.name}}
-                    </a>
+                    <div class="me-3"  @click="toggleTheme" role="button">
+                        
+                        <i v-if="isDarkMode" class="bi bi-brightness-high-fill"></i>
+                        <i v-else class="bi bi-moon-stars-fill"></i>
+                    </div>
+                    
 
-                    <ul class="dropdown-menu">
-                        <li><a class="dropdown-item" href="#">Sair</a></li>
-                    </ul>
+                    <div class="dropdown ">
+                        <a class="btn btn-light dropdown-toggle" :class="{'btn-dark': isDarkMode}" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                            {{company?.name}}
+                        </a>
+                        
+                        <ul class="dropdown-menu">
+                            <li><a class="dropdown-item" href="#">Sair</a></li>
+                        </ul>
+                    </div>
                 </div>
             </div>
         </div>
     </header>
-
+    
 </template>
+
+<style scoped>
+img {
+    max-width: 100%;
+    height: auto;
+}
+</style>
