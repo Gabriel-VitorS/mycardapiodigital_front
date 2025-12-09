2a70709749b3a9ecb2e45759dc6b5b8712d5d463
Gabriel Vitor <gabrielvitordasilva@gmail.com>
2025-12-09 17:25:36 -0300

    import do theme provide e layout admin

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,22 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import ThemeProvider from './components/layout/ThemeProvider.vue';
+import { useRoute } from 'vue-router';
+import { computed } from 'vue';
+import AdminLayout from './components/layout/AdminLayout.vue';
+
+const route = useRoute()
+
+const isAdm = computed(()=> route.path.includes("gestor"))
+</script>
 
 <template>
-  <router-view />
+  <ThemeProvider v-if="isAdm">
+    <AdminLayout>
+      <router-view />
+    </AdminLayout>
+  </ThemeProvider>
+
+  <router-view v-else />
 </template>
 
 <style scoped></style>
