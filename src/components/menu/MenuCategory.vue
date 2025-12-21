07487a159aee0df42ec8fcb2515b5f127e7774eb
Gabriel <gabrielvitordasilva@gmail.com>
2025-12-21 10:27:59 -0300

    componente do cardapio das categorias

---
+++ src/components/menu/MenuCategory.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="row" :id="`${categoryId}`">
+        <div class="col-12 my-3 p-2" >
+            <span class="fs-5 fw-medium" :style="{color: color}">{{ categoryName }}</span>
+        </div>
+    </div>
+</template>
+
+<script lang="ts" setup>
+
+defineProps<{
+    categoryName: string;
+    categoryId: number;
+    color: string;
+}>();
+
+</script>
