e713d67e40434110c88fcbd0b18b77745b495843
Gabriel <gabrielvitordasilva@gmail.com>
2026-01-21 09:07:35 -0300

    página inicial do gestor

---
+++ src/views/Manager/HomeView.vue
@@ -0,0 +1,34 @@
+<script lang="ts" setup>
+import BoxIcon from '@/components/icons/BoxIcon.vue';
+import GearIcon from '@/components/icons/GearIcon.vue';
+import TagsIcon from '@/components/icons/TagsIcon.vue';
+
+</script>
+
+<template>
+    <div class="container ">
+        <div class="row justify-content-center gy-2">
+            
+            <RouterLink to="/gestor/configuracao-cardapio" class="card col-12 col-md-12 me-2" style="width: 18rem;">
+                <div class="card-body text-center">
+                    <GearIcon  style="font-size: 2rem;"/>
+                    <p>Config Cardápio</p>
+                </div>
+            </RouterLink>
+
+            <RouterLink to="/gestor/categorias" class="card col-12 col-md-12 me-2" style="width: 18rem;">
+                <div class="card-body text-center">
+                    <TagsIcon  style="font-size: 2rem;"/>
+                    <p>Categorias</p>
+                </div>
+            </RouterLink>
+
+            <RouterLink to="/gestor/produtos" class="card col-12 col-md-12 me-2" style="width: 18rem;">
+                <div class="card-body text-center">
+                    <BoxIcon  style="font-size: 2rem;"/>
+                    <p>Produtos</p>
+                </div>
+            </RouterLink>
+        </div>
+    </div>
+</template>
